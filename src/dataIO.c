9b06cef60818a8bd868b21a08bfef1420b7e6a5d
mfratello <mfratello@485372f5-6e1a-4f42-b79e-b37274d68c60>
2014-07-21 14:38:08 +0000

    Initial commit: eps-improvement, neighborhood sampling

---
+++ src/dataIO.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE	4096
+#define ACC_SIZE	128
+
+void loadVector(FILE* f, char sep, double* vector, int n)
+{
+	char buff[BUFF_SIZE];
+	char accumulator[ACC_SIZE];
+	int k, accIdx, count;
+	int idx;
+
+	idx = 0;
+	accIdx=0;
+	while(count=fread(buff, sizeof(char), BUFF_SIZE, f))
+	{
+		for(k=0; k<count;k++)
+		{
+			if((buff[k]==sep) || (buff[k]=='\n'))
+			{
+				accumulator[accIdx]='\0';
+				vector[idx]=strtod(accumulator,NULL);
+				accIdx=0;
+				idx++;
+			}
+			else
+			{
+				accumulator[accIdx]=buff[k];
+				accIdx++;
+			}
+		}
+	}
+	return;
+}
+
+void storeVector(FILE* f, char sep, double* vector, int n)
+{
+	int i;
+	
+	for(i=0; i < (n-1); i++)
+	{
+		fprintf(f, "%lf%c", vector[i], sep);
+	}
+	fprintf(f, "%lf", vector[i]);
+	return;
+}
+
+void loadVectorINT(FILE* f, char sep, int* vector, int n)
+{
+	char buff[BUFF_SIZE];
+	char accumulator[ACC_SIZE];
+	int k, accIdx, count;
+	int idx;
+
+	idx = 0;
+	accIdx=0;
+	while(count=fread(buff, sizeof(char), BUFF_SIZE, f))
+	{
+		for(k=0; k<count;k++)
+		{
+			if((buff[k]==sep) || (buff[k]=='\n'))
+			{
+				accumulator[accIdx]='\0';
+				vector[idx]= (int) strtol(accumulator,NULL, 10);
+				accIdx=0;
+				idx++;
+			}
+			else
+			{
+				accumulator[accIdx]=buff[k];
+				accIdx++;
+			}
+		}
+	}
+	return;
+}
+
+void storeVectorINT(FILE* f, char sep, int* vector, int n)
+{
+	int i;
+	
+	for(i=0; i < (n-1); i++)
+	{
+		fprintf(f, "%d%c", vector[i], sep);
+	}
+	fprintf(f, "%d", vector[i]);
+	return;
+}
+
+void loadMatrix(FILE* f, char sep, double* matrix, int n, int m)
+{
+	char buff[BUFF_SIZE];
+	char accumulator[ACC_SIZE];
+	int i, j, k, accIdx, count;
+	int idx;
+
+	i = j = 0;
+	accIdx=0;
+	while(count=fread(buff, sizeof(char), BUFF_SIZE, f))
+	{
+		for(k=0; k<count;k++)
+		{
+			if((buff[k]==sep) || (buff[k]=='\n'))
+			{
+				accumulator[accIdx]='\0';
+				idx = i*m+j;
+				matrix[idx]=strtod(accumulator, NULL);
+				accIdx=0;
+				j++;
+				if(j == m) {j=0; i++;}
+			}
+			else
+			{
+				accumulator[accIdx]=buff[k];
+				accIdx++;
+			}
+		}
+	}
+	return;
+}
+
+void storeMatrix(FILE* f, char sep, double* matrix, int n, int m)
+{
+	int i, j;
+	int idx;
+	
+	for(i=0; i < n; i++)
+	{
+		for(j=0; j<(m-1); j++)
+		{
+			idx=i*m+j;
+			fprintf(f, "%lf%c", matrix[idx], sep);
+		}
+		idx=i*m+j;
+		fprintf(f, "%lf\n", matrix[idx]);
+	}
+	return;
+}
